be8846e6a629d12624ee89aa6f1d99e4a857fe37
Bob Engel <rae004@gmail.com>
2020-12-12 16:02:05 -0500

    removing local media, replaced with calls to s3 bucket rae-portfolio-storage151738-dev

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CardItem from "./CardItem";
 import './Cards.css';
+const CDN_SOURCE = 'https://rae-portfolio-storage151738-dev.s3.us-east-2.amazonaws.com';
 
 function Cards() {
     return (
@@ -10,13 +11,13 @@ function Cards() {
                 <div className={'cards__wrapper'}>
                     <ul className={'cards__items'}>
                         <CardItem
-                            src={'images/img-9.jpg'}
+                            src={CDN_SOURCE + 'images/img-9.jpg'}
                             text={'Explore the hidden wonders inside the CarItems'}
                             label={'Adventure'}
                             path={'/services'}
                         />
                         <CardItem
-                            src={'images/img-2.jpg'}
+                            src={CDN_SOURCE + 'images/img-2.jpg'}
                             text={'Beautiful card items inside!'}
                             label={'Adventure'}
                             path={'/services'}
@@ -24,19 +25,19 @@ function Cards() {
                     </ul>
                     <ul className={'cards__items'}>
                         <CardItem
-                            src={'images/img-4.jpg'}
+                            src={CDN_SOURCE + 'images/img-4.jpg'}
                             text={'Explore the hidden SUB wonders inside the CarSubItems'}
                             label={'Sub1'}
                             path={'/services'}
                         />
                         <CardItem
-                            src={'images/img-5.jpg'}
+                            src={CDN_SOURCE + 'images/img-5.jpg'}
                             text={'Beautiful SUB card items inside!'}
                             label={'Sub2'}
                             path={'/services'}
                         />
                         <CardItem
-                            src={'images/img-6.jpg'}
+                            src={CDN_SOURCE + 'images/img-6.jpg'}
                             text={'What a great SUB card!!!!'}
                             label={'Sub3'}
                             path={'/services'}
